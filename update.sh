1c14147742a64ea6e116ae96f36c249221955e9d
ghxc2 <42091882+ghxc2@users.noreply.github.com>
2023-10-02 02:10:15 -0400

    Working update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-git remote -v update
+
+git remote -v update > /dev/null 2>&1
+output="$(git status -uno)"
+
+if [[ $output == *"Your branch is behind"* ]]; then
+	echo "Time to update!"
+	git pull
+	pkill -f Lewdie.py
+	python3 Lewdie.py
+fi
